875042de0ea5259ee1b265079f824c37fc26f215
miruakasha19272 <miruakasha5@gmail.com>
2024-10-26 14:04:40 +0800

    owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -1,64 +1,66 @@
+const { GoatWrapper } = require('fca-liane-utils');
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-config: {
-  name: "owner",
-  aurthor:"Tokodori",// Convert By Goatbot T
-   role: 0,
-  shortDescription: " ",
-  longDescription: "",
-  category: "admin",
-  guide: "{pn}"
-},
+	config: {
+		name: "owner",
+		author: "Tokodori",
+		role: 0,
+		shortDescription: " ",
+		longDescription: "",
+		category: "admin",
+		guide: "{pn}"
+	},
 
-  onStart: async function ({ api, event }) {
-  try {
-    const ownerInfo = {
-      name: 'Matthew daniel',
-      gender: 'Male',
-      age: "16",
-      height: '5'6',
-      facebookLink: 'https://www.facebook.com/mattty152,
-      nick: 'matty'
-    };
+	onStart: async function ({ api, event }) {
+		try {
+			const ownerInfo = {
+				name: 'matthew daniel',
+				gender: 'male',
+				hobby: 'codm',
+				Fb: 'https://www.facebook.com/mattty152',
+				Relationship: 'ewan',
+				bio: 'dont know'
+			};
 
-    const bold = 'https://drive.google.com/uc?export=download&id=1YUJCt_RMN_VAQF4lrMcOv7qTSlrlD5Ak';
+			const bold = 'https://drive.google.com/uc?export=download&id=1YUJCt_RMN_VAQF4lrMcOv7qTSlrlD5Ak';
+			const tmpFolderPath = path.join(__dirname, 'tmp');
 
-    const tmpFolderPath = path.join(__dirname, 'tmp');
+			if (!fs.existsSync(tmpFolderPath)) {
+				fs.mkdirSync(tmpFolderPath);
+			}
 
-    if (!fs.existsSync(tmpFolderPath)) {
-      fs.mkdirSync(tmpFolderPath);
-    }
+			const videoResponse = await axios.get(bold, { responseType: 'arraybuffer' });
+			const videoPath = path.join(tmpFolderPath, 'owner_video.mp4');
 
-    const videoResponse = await axios.get(bold, { responseType: 'arraybuffer' });
-    const videoPath = path.join(tmpFolderPath, 'owner_video.mp4');
+			fs.writeFileSync(videoPath, Buffer.from(videoResponse.data, 'binary'));
 
-    fs.writeFileSync(videoPath, Buffer.from(videoResponse.data, 'binary'));
-
-    const response = `
-Owner Information:🧾
+			const response = `
+◈ 𝖮𝖶𝖭𝖤𝖱 𝖨𝖭𝖥𝖮𝖱𝖬𝖠𝖳𝖨𝖮𝖭:\n
 Name: ${ownerInfo.name}
 Gender: ${ownerInfo.gender}
-Age: ${ownerInfo.age}
-Height: ${ownerInfo.height}
-Facebook: ${ownerInfo.facebookLink}
-Nick: ${ownerInfo.nick}
-`;
+Relationship: ${ownerInfo.Relationship}
+Hobby: ${ownerInfo.hobby}
+Fb: ${ownerInfo.Fb}
+Bio: ${ownerInfo.bio}
+			`;
 
+			await api.sendMessage({
+				body: response,
+				attachment: fs.createReadStream(videoPath)
+			}, event.threadID, event.messageID);
 
-    await api.sendMessage({
-      body: response,
-      attachment: fs.createReadStream(videoPath)
-    }, event.threadID, event.messageID);
+			fs.unlinkSync(videoPath);
 
-    if (event.body.toLowerCase().includes('ownerinfo')) {
-      api.setMessageReaction('🪐', event.messageID, (err) => {}, true);
-    }
-  } catch (error) {
-    console.error('Error in ownerinfo command:', error);
-    return api.sendMessage('An error occurred while processing the command.', event.threadID);
-  }
-},
+			api.setMessageReaction('🚀', event.messageID, (err) => {}, true);
+		} catch (error) {
+			console.error('Error in ownerinfo command:', error);
+			return api.sendMessage('An error occurred while processing the command.', event.threadID);
+		}
+	}
 };
+
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
